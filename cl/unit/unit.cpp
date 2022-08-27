e0945b8cb61dfc64f4bb82ca59771a07ec85456e
GakmanAlexey <gakman@ya.ru>
2022-08-27 03:26:07 +0300

    save

---
+++ cl/unit/unit.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+class Unit
+{
+public:
+    int id = 0;//Ид юнита
+    std::string name = ""; //имя юнита
+    bool live = false; //Жив
+    bool online = false; //в сети
+
+};
